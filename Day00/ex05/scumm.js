63902f23d9b8ecd1666b784d63e10d53e062d72f
Gitshey <krivoshey.nikita1998@gmail.com>
2020-04-08 05:47:54 +0300

    Add ex05

---
+++ Day00/ex05/scumm.js
@@ -0,0 +1,36 @@
+var one = document.getElementById('TextInfo')
+
+function Pointer(element) {
+    element.innerHTML = "Hi! Use Hand and touch the girl :)"
+}
+function Hand(element) {
+    cursor: url(resources/loupe.png);
+    element.innerHTML = "Hoo";
+}
+function Eye(element) {
+    element.innerHTML = "Hi! Use Hand and touch the girl :)";
+}
+function Tool(element) {
+    element.innerHTML = "Hi! Use Hand and touch the girl :)";
+}
+function Dialog(element) {
+    element.innerHTML = "Hi! Use Hand and touch the girl :)";
+}
+function Book(element) {
+    element.innerHTML = "Hi! Use Hand and touch the girl :)";
+}
+function Towel(element) {
+    element.innerHTML = "Hi! Use Hand and touch the girl :)";
+}
+function Bricks(element) {
+    element.innerHTML = "Hi! Use Hand and touch the girl :)";
+}
+function Close(element) {
+    element.innerHTML = "Hi! Use Hand and touch the girl :)";
+}
+function Reload(element) {
+    element.innerHTML = "Hi! Use Hand and touch the girl :)";
+}
+function Day42(element) {
+    element.innerHTML = "Hi! Use Hand and touch the girl :)";
+}
