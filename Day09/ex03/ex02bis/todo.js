47307b6a8bc115e1ad9dadcf0eeb56c6417207f5
Gitshey <krivoshey.nikita1998@gmail.com>
2020-06-13 19:01:05 +0300

    ok

--- Day09/ex03/ex02bis/todo.js
+++ Day09/ex03/ex02bis/todo.js
@@ -1,26 +1,25 @@
 $(document).ready(function() {
-    $('#button').click(function(){
+    $("#button").click(function(){
         let content = prompt("To-do");
         let nbelem = getLastid();
-        let id_box = 'todo' + nbelem;
+        let id_box = "todo" + nbelem;
         if (content != null && content != "") {
-            jQuery('<div/>', {
+            jQuery("<div/>", {
                 id: id_box,
-                class: 'to_do_box',
+                class: "to_do_box",
                 text: content,
-                click: function(){
+                click: function() {
                     let ans = prompt("Del?", "y/n");
                     if (ans === "Y" || ans === "y" || ans === "yes" || ans === "Yes" || ans === "YES") {
                         delCookie(id_box, content);
-                        $('#' + id_box).remove();
+                        $("#" + id_box).remove();
                     }
                 }
-            }).prependTo('#ft_list');
+            }).prependTo("#ft_list");
             setCookie(id_box, content, 10800000);
         }
     });
 });
-
 function getLastid() {
     let i = 0;
     let point = "todo" + i;
@@ -34,45 +33,40 @@ function getLastid() {
     lastid++;
     return lastid;
 }
-
-
 function loadDiv (content, point) {
     if (content != null && content != "") {
-        jQuery('<div/>', {
+        jQuery("<div/>", {
             id: point,
-            class: 'to_do_box',
+            class: "to_do_box",
             text: content,
             click: function() {
                 let ans = prompt("Del?", "y/n");
                 if (ans === "Y" || ans === "y" || ans === "yes" || ans === "Yes" || ans === "YES") {
                     delCookie(point, content);
-                    $('#' + point).remove();
+                    $("#" + point).remove();
                 }
             }
-        }).prependTo('#ft_list');
+        }).prependTo("#ft_list");
     }
 }
-
 function setCookie(point, cvalue, time) {
     let d = new Date();
     d.setTime(d.getTime() + time);
     let expires = "expires="+ d.toUTCString();
     document.cookie = point + "=" + cvalue + "; " + expires;
 }
-
 function delCookie(point, cvalue) {
     let d = new Date();
     d.setTime(d.getTime() - (1000*60*60*24));
     let expires = "expires="+ d.toUTCString();
     document.cookie = point + "=" + cvalue + "; " + expires;
 }
-
 function getCookie(point) {
     let name = point + "=";
-    let ca = document.cookie.split(';');
+    let ca = document.cookie.split(";");
     for(let i = 0; i <ca.length; i++) {
         let c = ca[i];
-        while (c.charAt(0)==' ')
+        while (c.charAt(0)==" ")
             c = c.substring(1);
         if (c.indexOf(name) == 0)
             return c.substring(name.length,c.length);
